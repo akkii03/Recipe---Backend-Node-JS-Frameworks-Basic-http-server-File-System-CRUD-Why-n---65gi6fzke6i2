766f8144c088d6d660554db2842daa7b77d86843
akkii03 <akkiindoria72@gmail.com>
2022-09-10 01:32:55 +0530

    change database name to test

--- src/connector.js
+++ src/connector.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    database: "firstdb",
+    database: "test",
     multipleStatements: true
 });
 
