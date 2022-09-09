30c4ddee5c634a748e3a23c55f15635bfc776065
akkii03 <akkiindoria72@gmail.com>
2022-09-10 01:22:33 +0530

    solve

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
 const data = require("./data");
-const {connection} = require('./connector');
+const connection = require('./connector');
 
 
 // Parse JSON bodies (as sent by API clients)
@@ -12,48 +12,68 @@ app.use(bodyParser.json());
 
 
 app.get("/api/recipes",(req,res)=>{
-    res.send(data);
+    const sql = 'SELECT * FROM RECIPES';
+    connection.query(sql,(err,result)=>{
+        if(err) {
+            console.log("err due to ",err);
+            res.send({msg:"err"});
+        } else{
+            console.log("fetch successully");
+            res.send(result);
+        }
+    });
 })
 
 app.get("/api/recipes/:id",(req,res)=>{
-    const id = req.params.id;
-    const myObj = data.filter((item)=>item.id==id);
-    if(myObj) {
-        res.send(myObj);
-    }
-    else{
-        res.status(400).send({message: error.message});
-    }
+    const ids = req.params.id;
+    const sqlCmd = `SELECT * FROM RECIPES WHERE ID = ${ids}`;
+    connection.query(sqlCmd,(err,result)=>{
+        if(err) {
+
+            console.log("err due to ",err);
+            res.send("not found");
+        }else{
+            res.send(result);
+        }
+    })
 });
 
 app.post('/api/recipe',async(req,res)=>{
-    const {title,description,category,ingredients} = await req.body;
-    const sqlCmd = `INSERT INTO Recipes VALUES ('${title}','${description}','${category}','${ingredients}')`
+    const {id,title,description,category,ingredients} = await req.body;
+    const sqlCmd = `INSERT INTO Recipes VALUES (${id},'${title}','${description}','${category}','${ingredients}')`
     connection.query(sqlCmd,(err,result)=>{
         if(err) {
-            console.log("data is not inserted dut to ",err);
+            console.log("data is not inserted due to ",err);
         }else{
             console.log("data is inserted successfully");
+            res.send(req.body);
         }
     })
-    res.status(200).send(`recipe is inserted `);
 })
 
-app.put('/api/recipe/:id',(req,res)=>{
+app.put('/api/recipe/:id',async(req,res)=>{
     const idS = req.params.id;
-    const sqlCmd = `SELECT * FROM RECIPES WHERE ID = ${idS}`;
-    connection.query(sqlCmd,(err,result)=>{
+    const {id,title,description,category,ingredients} = await req.body;
+
+    const cmd = `UPDATE RECIPES SET ID = ${id},
+                TITLE = '${title}',
+                DESCRIPTION = '${description}',
+                CATEGORY = '${category}',
+                INGREDIENTS = '${ingredients}' WHERE ID = 13`;
+    connection.query(cmd,(err,result)=>{
         if(err) {
+            console.log("err due to ",err);
             res.status(400).send({message: "Recipe id is invalid"});
-        }else{
-            res.status(200).send({message: "Successfully updated a recipe"});   
+        } else{
+            console.log("value is updated");
+              res.send(req.body);
         }
     })
 });
 
 app.delete('/api/recipe/:id',(req,res)=>{
     const idS = req.params.id;
-    const sqlCmd = `SELECT * FROM RECIPES WHERE ID = ${idS}`;
+    const sqlCmd = `DELETE FROM RECIPES WHERE ID = ${idS}`;
     connection.query(sqlCmd,(err,result)=>{
         if(err) {
             res.status(400).send({message: "Recipe id is invalid or does not exist"});
